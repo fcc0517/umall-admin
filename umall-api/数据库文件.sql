c2441040e56542cfb2380cfecdbfc9d5e3a31434
fcc0517 <www.435561066@qq.com>
2021-02-20 18:00:18 +0800

    后台接口数据上传

---
+++ umall-api/数据库文件.sql
@@ -0,0 +1,218 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本地数据库
+Source Server Version : 50726
+Source Host           : localhost:3306
+Source Database       : umall-api-data
+
+Target Server Type    : MYSQL
+Target Server Version : 50726
+File Encoding         : 65001
+
+Date: 2021-01-26 16:53:36
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `banner`
+-- ----------------------------
+DROP TABLE IF EXISTS `banner`;
+CREATE TABLE `banner` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '轮播图id',
+  `title` varchar(50) NOT NULL COMMENT '轮播图标题',
+  `img` varchar(255) NOT NULL COMMENT '轮播图图片地址',
+  `status` tinyint(1) NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='轮播图表';
+
+-- ----------------------------
+-- Records of banner
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `cart`
+-- ----------------------------
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE `cart` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '购物车主键',
+  `uid` varchar(50) NOT NULL COMMENT '用户id',
+  `goodsid` int(11) NOT NULL COMMENT '商品编号',
+  `num` tinyint(3) NOT NULL COMMENT '数量',
+  `status` tinyint(1) NOT NULL COMMENT '选中状态',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `uid` (`uid`,`goodsid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='购物车表';
+
+-- ----------------------------
+-- Records of cart
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `category`
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类编号',
+  `pid` smallint(5) unsigned NOT NULL COMMENT '上级分类编号',
+  `catename` varchar(50) NOT NULL COMMENT '分类名称',
+  `img` varchar(255) NOT NULL COMMENT '分类图片',
+  `status` tinyint(1) unsigned NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品分类表';
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `goods`
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品编号',
+  `first_cateid` smallint(5) unsigned NOT NULL COMMENT '一级分类编号',
+  `second_cateid` smallint(6) NOT NULL COMMENT '二级分类编号',
+  `goodsname` varchar(100) NOT NULL COMMENT '商品名称',
+  `price` decimal(10,2) NOT NULL COMMENT '商品价格',
+  `market_price` decimal(10,2) NOT NULL COMMENT '市场价格',
+  `img` varchar(255) NOT NULL COMMENT '商品图片',
+  `description` text COMMENT '商品描述',
+  `specsid` int(11) NOT NULL COMMENT '规格id',
+  `specsattr` varchar(255) NOT NULL COMMENT '规则属性值',
+  `isnew` tinyint(1) NOT NULL COMMENT '是否新品1是2不是',
+  `ishot` tinyint(1) NOT NULL COMMENT '是否热卖1是2不是',
+  `status` tinyint(1) NOT NULL COMMENT '状态1启用2禁用',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品表';
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `member`
+-- ----------------------------
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` varchar(50) NOT NULL COMMENT '用户编号',
+  `phone` char(11) NOT NULL COMMENT '手机号',
+  `nickname` varchar(50) NOT NULL COMMENT '昵称',
+  `password` char(32) NOT NULL COMMENT '密码',
+  `randstr` char(5) NOT NULL COMMENT '密码随机串',
+  `addtime` char(13) NOT NULL COMMENT '注册时间',
+  `status` tinyint(1) NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='会员表';
+
+-- ----------------------------
+-- Records of member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '菜单编号',
+  `pid` smallint(6) NOT NULL COMMENT '上级菜单编号',
+  `title` varchar(50) NOT NULL COMMENT '菜单名称',
+  `icon` varchar(100) NOT NULL COMMENT '菜单图标',
+  `type` tinyint(1) NOT NULL COMMENT '菜单类型1目录2菜单',
+  `url` varchar(100) NOT NULL COMMENT '菜单地址',
+  `status` tinyint(1) NOT NULL COMMENT '菜单状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台菜单权限表';
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `role`
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '角色编号',
+  `rolename` varchar(100) NOT NULL COMMENT '角色名称',
+  `menus` varchar(255) NOT NULL COMMENT '菜单权限 存放的是菜单编号，用逗号隔开',
+  `status` tinyint(1) NOT NULL COMMENT '角色状态1正常2禁用',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台用户角色表';
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `seckill`
+-- ----------------------------
+DROP TABLE IF EXISTS `seckill`;
+CREATE TABLE `seckill` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '秒杀表id',
+  `title` varchar(100) NOT NULL COMMENT '活动名称',
+  `begintime` char(13) NOT NULL COMMENT '秒杀开始时间',
+  `endtime` char(13) NOT NULL COMMENT '秒杀结束时间',
+  `first_cateid` smallint(5) NOT NULL COMMENT '商品一级分类编号',
+  `second_cateid` smallint(5) NOT NULL COMMENT '商品二级分类编号',
+  `goodsid` int(11) NOT NULL COMMENT '商品编号',
+  `status` tinyint(1) NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='限时秒杀活动表';
+
+-- ----------------------------
+-- Records of seckill
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `specs`
+-- ----------------------------
+DROP TABLE IF EXISTS `specs`;
+CREATE TABLE `specs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '规格id',
+  `specsname` varchar(50) NOT NULL COMMENT '规格名称',
+  `status` tinyint(1) NOT NULL COMMENT '规格状态',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品规格表';
+
+-- ----------------------------
+-- Records of specs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `specs_attr`
+-- ----------------------------
+DROP TABLE IF EXISTS `specs_attr`;
+CREATE TABLE `specs_attr` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '规格项编号',
+  `specsid` int(11) NOT NULL COMMENT '规格的specsid',
+  `specsval` varchar(50) NOT NULL COMMENT '规格值',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品规格明细表';
+
+-- ----------------------------
+-- Records of specs_attr
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '住建',
+  `uid` varchar(50) NOT NULL COMMENT '管理员编号',
+  `roleid` smallint(5) NOT NULL COMMENT '角色编号',
+  `username` varchar(30) NOT NULL COMMENT '用户名',
+  `password` char(32) NOT NULL COMMENT '密码',
+  `randstr` char(5) NOT NULL COMMENT '随机加密串',
+  `status` tinyint(1) NOT NULL COMMENT '状态',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `uid` (`uid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台管理员用户表';
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', '4dc24a90-cd83-11ea-87be-2d0427c9aba4', '0', 'admin', '9f360b93367b75ad445d5cc4506755f1', '5pyiV', '1');
